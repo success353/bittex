304924f00e3bb97b305a4aa39ec615665f44756f
success353 <gmacro372@gmail.com>
2023-03-05 17:44:53 +0100

    modified:   components/Characters.jsx

--- components/Characters.jsx
+++ components/Characters.jsx
@@ -5,6 +5,9 @@ function Characters() {
   return (
     <div>
       <Back />
+      <div className='px-5 text-5xl font-bold'>
+        <h1 className="font-inter">Characters</h1>
+      </div>
     </div>
   );
 }
